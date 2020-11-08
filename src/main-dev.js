aa2210c5acf2b78ebf1ccdd5c910c849ffc87e97
呦西 <1016508470@qq.com>
2020-11-08 17:57:25 +0800

    完成

--- src/main-dev.js
+++ src/main-dev.js
@@ -11,12 +11,20 @@ import 'quill/dist/quill.core.css' // import styles
 import 'quill/dist/quill.snow.css' // for snow theme
 import 'quill/dist/quill.bubble.css' // for bubble theme
 
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+
 import axios from 'axios'
 // 配置请求的根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 axios.interceptors.request.use(config => {
-    config.headers.Authorization = window.sessionStorage.getItem('token')
-    return config
+  NProgress.start()
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
+axios.interceptors.response.use(config => {
+  NProgress.done()
+  return config
 })
 Vue.prototype.$http = axios
 
@@ -27,20 +35,20 @@ Vue.component('tree-table', TreeTable)
 Vue.use(VueQuillEditor)
 
 Vue.filter('dateFormat', function(originVal) {
-    const dt = new Date(originVal)
+  const dt = new Date(originVal)
 
-    const y = dt.getFullYear()
-    const m = (dt.getMonth() + 1 + '').padStart(2, '0')
-    const d = (dt.getDate() + '').padStart(2, '0')
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const d = (dt.getDate() + '').padStart(2, '0')
 
-    const hh = (dt.getHours() + '').padStart(2, '0')
-    const mm = (dt.getMinutes() + '').padStart(2, '0')
-    const ss = (dt.getSeconds() + '').padStart(2, '0')
+  const hh = (dt.getHours() + '').padStart(2, '0')
+  const mm = (dt.getMinutes() + '').padStart(2, '0')
+  const ss = (dt.getSeconds() + '').padStart(2, '0')
 
-    return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
 })
 
 new Vue({
-    router,
-    render: h => h(App)
+  router,
+  render: h => h(App)
 }).$mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+
 import Login from '../components/Login.vue'
+// const Login = () =>
+//     import ( /* webpackChunkName: "login_home_welcomw" */ '../components/Login.vu')
+
 import Home from '../components/Home.vue'
 import Welcome from '../components/Welcome.vue'
 import Users from '../components/user/Users.vue'
@@ -12,45 +16,57 @@ import GoodsList from '../components/goods/List.vue'
 import Add from '../components/goods/Add.vue'
 import Order from '../components/order/Order.vue'
 import Report from '../components/report/Report.vue'
+import { Message } from 'element-ui'
 
 Vue.use(VueRouter)
 
 // const routes = []
 const router = new VueRouter({
-    routes: [
-        { path: '/', redirect: '/login' },
-        { path: '/login', component: Login },
-        {
-            path: '/home',
-            component: Home,
-            redirect: '/welcome',
-            children: [
-                { path: '/welcome', component: Welcome },
-                { path: '/users', component: Users },
-                { path: '/rights', component: Rights },
-                { path: '/roles', component: Roles },
-                { path: '/categories', component: Cate },
-                { path: '/params', component: Params },
-                { path: '/goods', component: GoodsList },
-                { path: '/goods/add', component: Add },
-                { path: '/orders', component: Order },
-                { path: '/reports', component: Report }
-
-            ]
-        }
-    ]
+  routes: [
+    { path: '/', redirect: '/login' },
+    { path: '/login', name: 'login', component: Login },
+    {
+      path: '/home',
+      component: Home,
+      redirect: '/welcome',
+      children: [
+        { path: '/welcome', component: Welcome },
+        { path: '/users', component: Users },
+        { path: '/rights', component: Rights },
+        { path: '/roles', component: Roles },
+        { path: '/categories', component: Cate },
+        { path: '/params', component: Params },
+        { path: '/goods', component: GoodsList },
+        { path: '/goods/add', component: Add },
+        { path: '/orders', component: Order },
+        { path: '/reports', component: Report }
+      ]
+    }
+  ]
 })
 
 // 挂载路由导航守卫
 router.beforeEach((to, from, next) => {
-    // to 将要访问的路径
-    // from 代表从哪个路径跳转而来
-    // next 是一个函数,表示放行
-    // next() 放行  next('/login') 强制跳转
-    if (to.path === '/login') return next()
-    const tokenStr = window.sessionStorage.getItem('token')
-    if (!tokenStr) return next('/login')
+  // to 将要访问的路径
+  // from 代表从哪个路径跳转而来
+  // next 是一个函数,表示放行
+  // next() 放行  next('/login') 强制跳转
+
+  // if (to.path === '/login') return next()
+  // const tokenStr = window.sessionStorage.getItem('token')
+  // if (!tokenStr) return next('/login')
+  // next()
+  if (to.path === '/login') {
     next()
+  } else {
+    const tokenStr = window.sessionStorage.getItem('token')
+    if (!tokenStr) {
+      Message.warning('请先登录')
+      router.push({ name: 'login' })
+      return
+    }
+    next()
+  }
 })
 
 export default router
